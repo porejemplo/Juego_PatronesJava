88560ee57409d3a89fd2ee2c1867a9d548cb8b7b
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2021-05-06 20:34:48 +0200

    hola bb
    ejemplo de template method para la IA supongo

---
+++ src/AI1.java
@@ -0,0 +1,13 @@
+
+public class AI1 extends tempalteExplicacion{
+
+	@Override
+	public void swingAxe() {
+		// TODO Auto-generated method stub
+		//get.daño + decorator pattern de armar
+		/*
+		 Quitar vida al otro tio. 
+		 */
+	}
+	
+}
---
+++ src/tempalteExplicacion.java
@@ -0,0 +1,8 @@
+
+public abstract class tempalteExplicacion {
+	public void attack() {
+		//if enemigo esta cerca atacar
+		swingAxe();
+	}
+	public abstract void swingAxe();
+}
