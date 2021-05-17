537e48de4050a99c396b826a2a6718dba0a407af
Eduardo <57434811+NoobToDaMaximum@users.noreply.github.com>
2021-05-17 20:45:19 +0200

    Update FabricaEnemigos.java
    limpiar

--- src/Personajes/Enemigos/FabricaEnemigos.java
+++ src/Personajes/Enemigos/FabricaEnemigos.java
@@ -13,63 +13,47 @@ public class FabricaEnemigos {
 	//funcion que crea enemigos
 	public Enemigo crearEnemigo(catEnemigo enemigo) {
 		Random r = new Random();
-		int auxInt = r.nextInt(3);
+		int auxInt = r.nextInt(2);
 		//crea un enemigo aleatorio
 		switch (auxInt) {
 			case 0:
 				enemigo = catEnemigo.guerrero;
-				//una vez creado tambien se elige aleatoriamente la naturaleza del enemigo
-				if(auxInt % 2 == 0) {
-					Enemigo enemigoAgresivo = new EnemigoAgresivo(enemigo);
-					decorarEnemigo(enemigoAgresivo);
-					return enemigoAgresivo;
-				}else {
-					Enemigo enemigoPasivo = new EnemigoPasivo(enemigo);
-					decorarEnemigo(enemigoPasivo);
-					return enemigoPasivo;
-				}
+				return naturalezaEnemigo(enemigo);
 			case 1:
 				enemigo = catEnemigo.hechicero;
-				if(auxInt % 2 == 0) {
-					Enemigo enemigoAgresivo = new EnemigoAgresivo(enemigo);
-					decorarEnemigo(enemigoAgresivo);
-					return enemigoAgresivo;
-				}else {
-					Enemigo enemigoPasivo = new EnemigoPasivo(enemigo);
-					decorarEnemigo(enemigoPasivo);
-					return enemigoPasivo;
-				}
+				return naturalezaEnemigo(enemigo);
 			default:
 				enemigo = catEnemigo.mutante;
-				if(auxInt % 2 == 0) {
-					Enemigo enemigoAgresivo = new EnemigoAgresivo(enemigo);
-					decorarEnemigo(enemigoAgresivo);
-					return enemigoAgresivo;
-				}else {
-					Enemigo enemigoPasivo = new EnemigoPasivo(enemigo);
-					decorarEnemigo(enemigoPasivo);
-					return enemigoPasivo;
-				}
+				return naturalezaEnemigo(enemigo);
 		}
 	}
 	
+	//Elige aleatoriamente la naturaleza del enemigo
+	public Enemigo naturalezaEnemigo(catEnemigo enemigo) {
+		Random tipo = new Random();
+		int aux = tipo.nextInt(1);
+		
+		if(aux == 0) {
+			Enemigo enemigoAgresivo = new EnemigoAgresivo(enemigo);
+			decorarEnemigo(enemigoAgresivo);
+			return enemigoAgresivo;
+		}else {
+			Enemigo enemigoPasivo = new EnemigoPasivo(enemigo);
+			decorarEnemigo(enemigoPasivo);
+			return enemigoPasivo;
+		}
+	}
 	public void decorarEnemigo(Enemigo enemigo) {
 		// Por ultimo tenemos que llamar para que le den los decoradores.
-		// decorador de armas
+		// decorador de armas y decorador de armaduras
 		Random r = new Random();
 		int auxInt;
-		auxInt = r.nextInt(2);
+		auxInt = r.nextInt(1);
 		if (auxInt < 1) {
 			enemigo.addDecoradorDano(darArmaXArma());
-		} else {
-			enemigo.addDecoradorDano(darArmaXArmadura());
-		}
-
-		// Decorador de Armadura
-		auxInt = r.nextInt(2);
-		if (auxInt < 1) {
 			enemigo.addDecoradorVida(darArmaduraXArma());
 		} else {
+			enemigo.addDecoradorDano(darArmaXArmadura());
 			enemigo.addDecoradorVida(darArmaduraXArmadura());
 		}
 
@@ -80,7 +64,7 @@ public class FabricaEnemigos {
 			enemigo.getPociones().addAll(darObjetosAtaque());
 			enemigo.getPociones().addAll(darObjetosDefensa());
 		} else {
-			auxInt = r.nextInt(2);
+			auxInt = r.nextInt(1);
 			if (auxInt < 1) {
 				enemigo.getPociones().addAll(darObjetosAtaque());
 			} else {
